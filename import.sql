02abe81415e683bafb4982f32a4fb514e3d5a062
Ryan Sutrisno <ryan.sutrisno@student.rocva.nl>
2024-11-04 09:55:56 +0100

    database

--- import.sql
+++ import.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS `ravendale`;
+
+CREATE DATABASE `ravendale`;
+
+USE `ravendale`;
+
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
